910e447147feb0782c3907c4647126b368471fa8
yliang9 <yliang9@gmail.com>
2019-09-02 16:35:32 -0400

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { StoreModule } from '@ngrx/store';
-import { fibReducer} from './fib.reducer';
+import * as FibReducer from './fib.reducer';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ,
-     StoreModule.forRoot({ fib: fibReducer })
+     StoreModule.forRoot(FibReducer.reducer)
   ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
